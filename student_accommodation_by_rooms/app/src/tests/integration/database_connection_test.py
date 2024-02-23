60e688e70a866d72d8c22ff339d6dfffb7bf0a95
anastasiya1497 <apenko.nast@gmail.com>
2024-02-23 01:28:12 +0100

    test: create integration tests.

---
+++ student_accommodation_by_rooms/app/src/tests/integration/database_connection_test.py
@@ -0,0 +1,26 @@
+import unittest
+from app.src.students_accommodation.db_services.connectors.mysql_connector import connect_to_mysql
+
+
+class TestDatabaseConnection(unittest.TestCase):
+    def test_mysql_connection(self):
+        config = {
+            "user": "testUSER",
+            "password": "testPWD",
+            "host": "mysql",
+            "port": 3306,
+            "database": "testDB"
+        }
+
+        db_connection = connect_to_mysql(config)
+        self.assertIsNotNone(db_connection)
+
+        cursor = db_connection.cursor()
+        cursor.execute("SELECT 1")
+        self.assertEqual(cursor.fetchone()[0], 1)
+
+        db_connection.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
